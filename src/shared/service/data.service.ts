4183426f52107d576d59138747b98cd739797650
kwonseoyoon <seoyoon24@megabridge.co.kr>
2023-04-04 15:58:36 +0900

    db service 생성

--- src/shared/service/data.service.ts
+++ src/shared/service/data.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class DataService {
 
   constructor(private http: HttpClient) { }
 
   getData(): Observable<any> {
-    return this.http.get<any>('../../assets/data.json');
+    return this.http.get("http://localhost:3000/posts");
   }
 }
