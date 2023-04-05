ee9a4fe874de9228f8ac932ff3fb38d0cbb1cfc7
kwonseoyoon <seoyoon24@megabridge.co.kr>
2023-04-05 18:13:50 +0900

    login 사용자 확인 동작 추가

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,30 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import { Router } from '@angular/router';
+import {DataService} from "../../shared/service/data.service";
+import {NotificationService} from "@progress/kendo-angular-notification";
+import {UserResponseModel} from "../../shared/model/response/user.response.model";
+
+type HorizontalPosition = "left" | "center" | "right";
+type VerticalPosition = "top" | "bottom";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
+  public loginData: UserResponseModel[] = [];
+
+  public email:string='';
+  public passWord:string='';
+
+  public horizontal: HorizontalPosition = "center";
+  public vertical: VerticalPosition = "top";
+
   selectedStyle = 'login-page-wrap';
 
   /**
    * @param router
+   * @param dataService
+   * @param notificationService
    */
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private dataService: DataService,
+    private notificationService: NotificationService
+  ) {}
 
   /**
    * 초기화
    */
-  ngOnInit() {}
+  ngOnInit() {
+    this.dataService.getUserData().subscribe(data => {
+      this.loginData = data;
+    })
+  }
 
   /**
    * 로그인 시 대시보드로 이동 이벤트
    */
   onLogin() {
-    localStorage.setItem('isLoggedin', 'true');
-    this.router.navigate(['/dashboard']);
+    this.loginData.forEach(item => {
+        if(item.email === this.email ){
+          if(item.password === this.passWord){
+            this.router.navigateByUrl(`/dashboard`);
+            return
+          }
+          else{
+            this.openAlert('비밀번호를 확인해주세요.');
+          }
+        }
+        else{
+          this.openAlert('존재하지 않는 Email입니다.');
+        }
+      });
+  }
+
+
+  openAlert(message: string) {
+    this.notificationService.show({
+      content: message,
+      cssClass: 'button-notification',
+      animation: { type: "fade", duration: 300 },
+      type: { style: "info", icon: true },
+      position: { horizontal: this.horizontal, vertical: this.vertical },
+      width: 500,
+      height: 50,
+      hideAfter:1500,
+    });
   }
 
 }
